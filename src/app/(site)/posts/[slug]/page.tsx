a0444bfb2f7e81a1bf075db049e9764211e82d5c
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-28 23:17:15 +0200

    update

--- src/app/(site)/posts/[slug]/page.tsx
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -1,43 +1,7 @@
-import { Post } from "@/lib/types/post";
-import client from "@/lib/contentful";
-import { notFound } from "next/navigation";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Image from "next/image";
-
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const { slug } = params;
-
-  const res = await client.getEntries<Post>({
-    content_type: "blogPost",
-    "fields.slug": slug,
-  });
-
-  const post = res.items[0];
-
-  if (!post) return notFound();
-
-  const { title, publishDate, content, image } = post.fields;
-
+export default function Page() {
   return (
-    <main className="max-w-3xl mx-auto px-4 py-10">
-      <h1 className="text-4xl font-bold mb-4">{title}</h1>
-      <p className="text-gray-500 mb-4">{publishDate}</p>
-      {image && (
-        <Image
-          src={`https:${image.fields.file.url}`}
-          alt={title}
-          width={800}
-          height={400}
-          className="w-full mb-6 rounded"
-        />
-      )}
-      <div className="prose">{documentToReactComponents(content)}</div>
-    </main>
+    <div>
+      <h1>test single post page </h1>
+    </div>
   );
 }
