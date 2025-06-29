8846a984c1eccc9feba1bb0a370bbb7eb09d4ef9
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-29 15:40:11 +0200

    update

--- src/app/(site)/posts/[slug]/page.tsx
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -3,13 +3,13 @@ import { notFound } from "next/navigation";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
 
-type Props = {
+interface PostPageProps {
   params: {
     slug: string;
   };
-};
+}
 
-export default async function PostPage({ params }: Props) {
+export default async function PostPage({ params }: PostPageProps) {
   const { slug } = params;
 
   const res = await client.getEntries({
@@ -20,33 +20,29 @@ export default async function PostPage({ params }: Props) {
   const post = res.items[0];
 
   if (!post) {
-    notFound(); // 404 pagina
+    notFound();
   }
 
   const { title, publishDate, content, image } = post.fields;
 
   return (
-      <article>
-        <h1>Show post page</h1>
-        <h1>{title}</h1>
-        <p className="text-gray-500 mb-4">{publishDate}</p>
+    <article>
+      <h1>{title}</h1>
+      <p className="text-gray-500 mb-4">{publishDate}</p>
 
-        {image && (
-          // <img
-          //   src={`https:${image.fields.file.url}`}
-          //   alt={title}
-          //   className="w-full mb-6 rounded"
-          // />
-          <Image
-    src={`https:${image.fields.file.url}`}
-    alt={title}
-     width={image.fields.file.details.image.width}
-    height={image.fields.file.details.image.height}
-    className="w-full max-w-md mb-4 rounded"
-  />
+      {image && (
+        <Image
+          src={`https:${image.fields.file.url}`}
+          alt={title}
+          width={image.fields.file.details.image.width}
+          height={image.fields.file.details.image.height}
+          className="w-full max-w-md mb-4 rounded"
+        />
+      )}
 
-        )}
-      <div className="prose">{documentToReactComponents(content)}</div>
-      </article>
+      <div className="prose">
+        {documentToReactComponents(content)}
+      </div>
+    </article>
   );
 }
