6e964fa6276c5644a4c9df4bdbfa97c10bc5e5f9
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-29 15:58:19 +0200

    update

--- src/app/(site)/posts/page.tsx
+++ src/app/(site)/posts/page.tsx
@@ -1,7 +1,24 @@
-export default function BlogPage() {
+import client from '@/lib/contentful';
+import { Post } from '@/lib/types/post';
+
+export default async function BlogPage() {
+  const res = await client.getEntries<Post>({
+    content_type: 'blogPost',
+    order: '-fields.publishDate',
+  });
+
+  const posts = res.items;
+
   return (
     <div>
       <h1>All post page</h1>
+      <ul className="flex flex-col gap-10 list-none">
+        {posts.map(({ sys, fields }) => (
+          <li key={sys.id} className="border-b pb-6">
+            <h2 className="text-2xl font-bold">{fields.title}</h2>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/lib/types/post.ts
+++ src/lib/types/post.ts
@@ -1,12 +1,33 @@
-import { Document } from "@contentful/rich-text-types";
+// import { Document } from "@contentful/rich-text-types";
 
-export type Post = {
-  sys: { id: string };
-  fields: {
-    title: string;
-    publishDate: string;
-    slug: string;
-    content: Document;
-    image?: { fields: { file: { url: string } } };
+// export type Post = {
+//   sys: { id: string };
+//   fields: {
+//     title: string;
+//     publishDate: string;
+//     slug: string;
+//     content: Document;
+//     image?: { fields: { file: { url: string } } };
+//   };
+// };
+
+// types/post.ts
+export interface Post {
+  title: string;
+  slug: string;
+  content: any;
+  publishDate: string;
+  image?: {
+    fields: {
+      file: {
+        url: string;
+        details: {
+          image: {
+            width: number;
+            height: number;
+          };
+        };
+      };
+    };
   };
-};
+}
