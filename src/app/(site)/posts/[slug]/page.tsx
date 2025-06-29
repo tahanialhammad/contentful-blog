e269f41fad1c6f3f71fb40b5318e7ae700949f47
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-29 16:05:52 +0200

    update

---
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import client from "@/lib/contentful";
+import { notFound } from "next/navigation";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { Post } from '@/lib/types/post';
+
+
+export default async function PostPage({ params }) {
+  const { slug } = params;
+
+  const res = await client.getEntries<Post>({
+    content_type: "blogPost",
+    "fields.slug": slug,
+  });
+
+  const post = res.items[0];
+
+  if (!post) {
+    notFound(); // 404 pagina
+  }
+
+  const { title, publishDate, content, image } = post.fields;
+
+  return (
+      <article>
+        <h1>Show post page</h1>
+        <h1>{title}</h1>
+        <p className="text-gray-500 mb-4">{publishDate}</p>
+
+        {image && (
+          <img
+            src={`https:${image.fields.file.url}`}
+            alt={title}
+            className="w-full mb-6 rounded"
+          />
+        )}
+      <div className="prose">{documentToReactComponents(content)}</div>
+      </article>
+  );
+}
--- src/app/(site)/posts/page.tsx
+++ src/app/(site)/posts/page.tsx
@@ -1,24 +1,35 @@
 import client from '@/lib/contentful';
+import Link from 'next/link';
 import { Post } from '@/lib/types/post';
 
-export default async function BlogPage() {
-  const res = await client.getEntries<Post>({
+async function getPosts() {
+  const response = await client.getEntries<Post>({
     content_type: 'blogPost',
     order: '-fields.publishDate',
   });
+  return response.items;
+}
 
-  const posts = res.items;
+export default async function PostsPage() {
+  const posts = await getPosts();
 
   return (
-    <div>
-      <h1>All post page</h1>
+    <main>
+      <h1 className="text-3xl font-bold mb-8">Mijn Blog</h1>
       <ul className="flex flex-col gap-10 list-none">
-        {posts.map(({ sys, fields }) => (
-          <li key={sys.id} className="border-b pb-6">
-            <h2 className="text-2xl font-bold">{fields.title}</h2>
+        {posts.map((post) => (
+          <li key={post.sys.id}>
+            <h2 className="text-2xl font-bold">
+              <Link href={`/posts/${post.fields.slug}`}>
+                {post.fields.title}
+              </Link>
+            </h2>
+            <p className="text-gray-600">
+              Gepubliceerd op: {new Date(post.fields.publishDate).toLocaleDateString()}
+            </p>
           </li>
         ))}
       </ul>
-    </div>
+    </main>
   );
 }
