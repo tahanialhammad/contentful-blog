7756848974fb2e640a32bc71aa66462a66d68d5f
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-29 15:21:50 +0200

    update

--- src/app/(site)/posts/page.tsx
+++ src/app/(site)/posts/page.tsx
@@ -1,35 +1,7 @@
-import client from '@/lib/contentful';
-import Link from 'next/link';
-import { Post } from '@/lib/types/post';
-
-async function getPosts() {
-  const response = await client.getEntries<Post>({
-    content_type: 'blogPost',
-    order: '-fields.publishDate',
-  });
-  return response.items;
-}
-
-export default async function PostsPage() {
-  const posts = await getPosts();
-
+export default function BlogPage() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold mb-8">Mijn Blog</h1>
-      <ul className="flex flex-col gap-10 list-none">
-        {posts.map((post) => (
-          <li key={post.sys.id}>
-            <h2 className="text-2xl font-bold">
-              <Link href={`/posts/${post.fields.slug}`}>
-                {post.fields.title}
-              </Link>
-            </h2>
-            <p className="text-gray-600">
-              Gepubliceerd op: {new Date(post.fields.publishDate).toLocaleDateString()}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div>
+      <h1>All post page</h1>
+    </div>
   );
 }
