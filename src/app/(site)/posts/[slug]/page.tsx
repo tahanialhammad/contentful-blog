484999bcba48da9c46b4dbc7444b4a0b51ea05cd
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-06-29 14:18:51 +0200

    update

--- src/app/(site)/posts/[slug]/page.tsx
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import { notFound } from "next/navigation";
 import client from "@/lib/contentful";
+import { notFound } from "next/navigation";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { PostSkeleton } from "@/lib/types/post";
+import { Post } from '@/lib/types/post';
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
+
+export default async function PostPage({ params }) {
   const { slug } = params;
 
-  const res = await client.getEntries<PostSkeleton>({
+  const res = await client.getEntries<Post>({
     content_type: "blogPost",
     "fields.slug": slug,
   });
@@ -14,24 +15,25 @@ export default async function PostPage({ params }: { params: { slug: string } })
   const post = res.items[0];
 
   if (!post) {
-    notFound();
+    notFound(); // 404 pagina
   }
 
   const { title, publishDate, content, image } = post.fields;
 
   return (
-    <article>
-      <h1>{title}</h1>
-      <p className="text-gray-500 mb-4">{publishDate}</p>
+      <article>
+        <h1>Show post page</h1>
+        <h1>{title}</h1>
+        <p className="text-gray-500 mb-4">{publishDate}</p>
 
-      {image && (
-        <img
-          src={`https:${image.fields.file.url}`}
-          alt={title}
-          className="w-full mb-6 rounded"
-        />
-      )}
+        {image && (
+          <img
+            src={`https:${image.fields.file.url}`}
+            alt={title}
+            className="w-full mb-6 rounded"
+          />
+        )}
       <div className="prose">{documentToReactComponents(content)}</div>
-    </article>
+      </article>
   );
 }
